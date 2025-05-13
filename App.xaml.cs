3c854555727d78c600786ba75258ad5aa6bea35e
Maxence CHUTAUX <darksas924@gmail.com>
2025-05-13 18:41:40 +0200

    template of Luigi.WpfTemplate / artemigos for creating a wpf project

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace TSD_WPF
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+    }
+}
